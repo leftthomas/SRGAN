15ac98927061a9745b968dcadb2cd218a7bc1ce9
leftthomas <leftthomas@qq.com>
2017-11-06 20:16:21 +0800

    slice

--- capsnet.py
+++ capsnet.py
@@ -1,22 +1,14 @@
-"""
-Dynamic Routing Between Capsules
-https://arxiv.org/abs/1710.09829
-PyTorch implementation by Kenta Iwasaki @ Gram.AI.
-"""
-
 import torch
 import torch.nn.functional as F
 from torch import nn
+from torch.autograd import Variable
 
-BATCH_SIZE = 100
-NUM_CLASSES = 10
-NUM_EPOCHS = 30
-NUM_ROUTING_ITERATIONS = 3
+import Config
 
 
 class CapsuleLayer(nn.Module):
     def __init__(self, num_capsules, num_route_nodes, in_channels, out_channels, kernel_size=None, stride=None,
-                 num_iterations=NUM_ROUTING_ITERATIONS):
+                 num_iterations=Config.NUM_ROUTING_ITERATIONS):
         super(CapsuleLayer, self).__init__()
 
         self.num_route_nodes = num_route_nodes
@@ -70,11 +62,11 @@ class CapsuleNet(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=256, kernel_size=9, stride=1)
         self.primary_capsules = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=256, out_channels=32,
                                              kernel_size=9, stride=2)
-        self.digit_capsules = CapsuleLayer(num_capsules=NUM_CLASSES, num_route_nodes=32 * 6 * 6, in_channels=8,
+        self.digit_capsules = CapsuleLayer(num_capsules=Config.NUM_CLASSES, num_route_nodes=32 * 6 * 6, in_channels=8,
                                            out_channels=16)
 
         self.decoder = nn.Sequential(
-            nn.Linear(16 * NUM_CLASSES, 512),
+            nn.Linear(16 * Config.NUM_CLASSES, 512),
             nn.ReLU(inplace=True),
             nn.Linear(512, 1024),
             nn.ReLU(inplace=True),
@@ -94,146 +86,13 @@ class CapsuleNet(nn.Module):
             # In all batches, get the most active capsule.
             _, max_length_indices = classes.max(dim=1)
             if torch.cuda.is_available():
-                y = Variable(torch.sparse.torch.eye(NUM_CLASSES)).cuda().index_select(dim=0,
-                                                                                      index=max_length_indices.data)
+                y = Variable(torch.sparse.torch.eye(Config.NUM_CLASSES)).cuda().index_select(dim=0,
+                                                                                             index=max_length_indices.data)
             else:
-                y = Variable(torch.sparse.torch.eye(NUM_CLASSES)).index_select(dim=0, index=max_length_indices.data)
+                y = Variable(torch.sparse.torch.eye(Config.NUM_CLASSES)).index_select(dim=0,
+                                                                                      index=max_length_indices.data)
         reconstructions = self.decoder((x * y[:, :, None]).view(x.size(0), -1))
 
         return classes, reconstructions
 
 
-if __name__ == "__main__":
-    from torch.autograd import Variable
-    from torch.optim import Adam
-    from torchnet.engine import Engine
-    from torchnet.logger import VisdomPlotLogger, VisdomLogger
-    from torchvision.utils import make_grid
-    from torchvision.datasets.mnist import MNIST
-    from tqdm import tqdm
-    import torchnet as tnt
-    from loss import CapsuleLoss
-    import utils
-
-    model = CapsuleNet()
-    if torch.cuda.is_available():
-        model.cuda()
-
-    print("# parameters:", sum(param.numel() for param in model.parameters()))
-
-    optimizer = Adam(model.parameters())
-
-    engine = Engine()
-    meter_loss = tnt.meter.AverageValueMeter()
-    meter_accuracy = tnt.meter.ClassErrorMeter(accuracy=True)
-    confusion_meter = tnt.meter.ConfusionMeter(NUM_CLASSES, normalized=True)
-
-    train_loss_logger = VisdomPlotLogger('line', opts={'title': 'Train Loss'})
-    train_error_logger = VisdomPlotLogger('line', opts={'title': 'Train Accuracy'})
-    test_loss_logger = VisdomPlotLogger('line', opts={'title': 'Test Loss'})
-    test_accuracy_logger = VisdomPlotLogger('line', opts={'title': 'Test Accuracy'})
-    confusion_logger = VisdomLogger('heatmap', opts={'title': 'Confusion matrix',
-                                                     'columnnames': list(range(NUM_CLASSES)),
-                                                     'rownames': list(range(NUM_CLASSES))})
-    ground_truth_logger = VisdomLogger('image', opts={'title': 'Ground Truth'})
-    reconstruction_logger = VisdomLogger('image', opts={'title': 'Reconstruction'})
-
-    capsule_loss = CapsuleLoss()
-
-
-    def get_iterator(mode):
-        dataset = MNIST(root='./data', download=True, train=mode)
-        data = getattr(dataset, 'train_data' if mode else 'test_data')
-        labels = getattr(dataset, 'train_labels' if mode else 'test_labels')
-        tensor_dataset = tnt.dataset.TensorDataset([data, labels])
-
-        return tensor_dataset.parallel(batch_size=BATCH_SIZE, num_workers=4, shuffle=mode)
-
-
-    def processor(sample):
-        data, labels, training = sample
-
-        data = utils.augmentation(data.unsqueeze(1).float() / 255.0)
-        labels = torch.LongTensor(labels)
-
-        labels = torch.sparse.torch.eye(NUM_CLASSES).index_select(dim=0, index=labels)
-
-        data = Variable(data)
-        labels = Variable(labels)
-        if torch.cuda.is_available():
-            data = data.cuda()
-            labels = labels.cuda()
-
-        if training:
-            classes, reconstructions = model(data, labels)
-        else:
-            classes, reconstructions = model(data)
-
-        loss = capsule_loss(data, labels, classes, reconstructions)
-
-        return loss, classes
-
-
-    def reset_meters():
-        meter_accuracy.reset()
-        meter_loss.reset()
-        confusion_meter.reset()
-
-
-    def on_sample(state):
-        state['sample'].append(state['train'])
-
-
-    def on_forward(state):
-        meter_accuracy.add(state['output'].data, torch.LongTensor(state['sample'][1]))
-        confusion_meter.add(state['output'].data, torch.LongTensor(state['sample'][1]))
-        meter_loss.add(state['loss'].data[0])
-
-
-    def on_start_epoch(state):
-        reset_meters()
-        state['iterator'] = tqdm(state['iterator'])
-
-
-    def on_end_epoch(state):
-        print('[Epoch %d] Training Loss: %.4f (Accuracy: %.2f%%)' % (
-            state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0]))
-
-        train_loss_logger.log(state['epoch'], meter_loss.value()[0])
-        train_error_logger.log(state['epoch'], meter_accuracy.value()[0])
-
-        reset_meters()
-
-        engine.test(processor, get_iterator(False))
-        test_loss_logger.log(state['epoch'], meter_loss.value()[0])
-        test_accuracy_logger.log(state['epoch'], meter_accuracy.value()[0])
-        confusion_logger.log(confusion_meter.value())
-
-        print('[Epoch %d] Testing Loss: %.4f (Accuracy: %.2f%%)' % (
-            state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0]))
-
-        torch.save(model.state_dict(), 'epochs/epoch_%d.pt' % state['epoch'])
-
-        # Reconstruction visualization.
-
-        test_sample = next(iter(get_iterator(False)))
-
-        ground_truth = (test_sample[0].unsqueeze(1).float() / 255.0)
-        if torch.cuda.is_available():
-            _, reconstructions = model(Variable(ground_truth).cuda())
-        else:
-            _, reconstructions = model(Variable(ground_truth))
-        reconstruction = reconstructions.cpu().view_as(ground_truth).data
-
-        ground_truth_logger.log(
-            make_grid(ground_truth, nrow=int(BATCH_SIZE ** 0.5), normalize=True, range=(0, 1)).numpy())
-        reconstruction_logger.log(
-            make_grid(reconstruction, nrow=int(BATCH_SIZE ** 0.5), normalize=True, range=(0, 1)).numpy())
-
-
-    engine.hooks['on_sample'] = on_sample
-    engine.hooks['on_forward'] = on_forward
-    engine.hooks['on_start_epoch'] = on_start_epoch
-    engine.hooks['on_end_epoch'] = on_end_epoch
-
-    engine.train(processor, get_iterator(True), maxepoch=NUM_EPOCHS, optimizer=optimizer)
