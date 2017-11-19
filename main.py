6f14a18ea65272d6dc3d1e1d72c2034d8bc1cf53
leftthomas <leftthomas@qq.com>
2017-11-19 16:52:43 +0800

    simplified the GAN train

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import argparse
+
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data
-import argparse
-from data_utils import DatasetFromFolder
 import torchvision.transforms as transforms
 import torchvision.utils as utils
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from model import Discriminator, Generator
 from tqdm import tqdm
+
+from data_utils import DatasetFromFolder
+from model import Discriminator, Generator
 
 parser = argparse.ArgumentParser(description='Train Super Resolution')
 parser.add_argument('--upscale_factor', default=3, type=int, help='super resolution upscale factor')
@@ -43,59 +45,57 @@ for epoch in range(NUM_EPOCHS):
     index = 1
     for data, target in bar:
         batch_size = data.size(0)
+        real_label = Variable(torch.ones(batch_size))
+        fake_label = Variable(torch.zeros(batch_size))
+
         ############################
         # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
         ###########################
-        # train with real
-        netD.zero_grad()
-        real_label = torch.Tensor(batch_size * [1])
+        real_img = Variable(target)
         if torch.cuda.is_available():
-            target = target.cuda()
+            real_img = real_img.cuda()
             real_label = real_label.cuda()
-        real = Variable(target)
-        real_label = Variable(real_label)
 
-        real_output = netD(real)
-        errD_real = criterion(real_output, real_label)
-        errD_real.backward()
-        D_x = real_output.data.mean()
+        # compute loss of real_img
+        real_out = netD(real_img)
+        d_loss_real = criterion(real_out, real_label)
+        real_scores = real_out.data.mean()
 
-        # train with fake
-        fake_label = torch.Tensor(batch_size * [0])
+        # compute loss of fake_img
+        z = Variable(data)
         if torch.cuda.is_available():
-            data = data.cuda()
+            z = z.cuda()
             fake_label = fake_label.cuda()
-        fake = Variable(data)
-        fake = netG(fake)
-        fake_label = Variable(fake_label)
-        fake_output = netD(fake.detach())
-        errD_fake = criterion(fake_output, fake_label)
-        errD_fake.backward()
-        D_G_z1 = fake_output.data.mean()
+        fake_img = netG(z)
+        fake_out = netD(fake_img)
+        d_loss_fake = criterion(fake_out, fake_label)
+        fake_scores = fake_out.data.mean()
 
-        errD = errD_real + errD_fake
+        # bp and optimize
+        d_loss = d_loss_real + d_loss_fake
+        netD.zero_grad()
+        d_loss.backward()
         optimizerD.step()
 
         ############################
         # (2) Update G network: maximize log(D(G(z)))
         ###########################
-        netG.zero_grad()
-        # fake labels are real for generator cost
-        label = torch.Tensor(batch_size * [1])
-        if torch.cuda.is_available():
-            label = label.cuda()
-        output = netD(fake)
-        errG = criterion(output, label)
-        errG.backward()
-        D_G_z2 = output.data.mean()
+        # compute loss of fake_img
+        fake_img = netG(z)
+        output = netD(fake_img)
+        g_loss = criterion(output, real_label)
+
+        # bp and optimize
+        optimizerG.zero_grad()
+        g_loss.backward()
         optimizerG.step()
 
-        bar.set_description(desc='[%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-                                 % (epoch + 1, NUM_EPOCHS, errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2))
+        bar.set_description(desc='[%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f'
+                                 % (epoch + 1, NUM_EPOCHS, d_loss.data[0], g_loss.data[0], real_scores, fake_scores))
 
         if index % 100 == 0:
             utils.save_image(target, '%s/real_samples.png' % 'images', normalize=True)
-            fake = netG(Variable(data))
+            fake = netG(z)
             utils.save_image(fake.data, '%s/fake_samples_epoch_%d.png' % ('images', epoch), normalize=True)
 
     # save model parameters
