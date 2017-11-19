d01c2000f8720a97bf86d585b5ad95332c37f733
leftthomas <leftthomas@qq.com>
2017-11-19 11:04:27 +0800

    fix image size bug

--- model.py
+++ model.py
@@ -25,25 +25,25 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(100, 64 * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(64 * 8),
+            nn.ConvTranspose2d(100, 32 * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(32 * 8),
             nn.ReLU(True),
-            # state size. (64*8) x 4 x 4
-            nn.ConvTranspose2d(64 * 8, 64 * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 4),
+            # state size. (32*8) x 4 x 4
+            nn.ConvTranspose2d(32 * 8, 32 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32 * 4),
             nn.ReLU(True),
-            # state size. (64*4) x 8 x 8
-            nn.ConvTranspose2d(64 * 4, 64 * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 2),
+            # state size. (32*4) x 8 x 8
+            nn.ConvTranspose2d(32 * 4, 32 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32 * 2),
             nn.ReLU(True),
-            # state size. (64*2) x 16 x 16
-            nn.ConvTranspose2d(64 * 2, 64, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64),
+            # state size. (32*2) x 16 x 16
+            nn.ConvTranspose2d(32 * 2, 32, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32),
             nn.ReLU(True),
-            # state size. (64) x 32 x 32
-            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
+            # state size. (32) x 32 x 32
+            nn.ConvTranspose2d(32, 3, 4, 2, 1, bias=False),
             nn.Tanh()
-            # state size. (3) x 64 x 64
+            # state size. (3) x 32 x 32
         )
 
     def forward(self, x):
@@ -55,23 +55,23 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.main = nn.Sequential(
-            # input is (3) x 64 x 64
-            nn.Conv2d(3, 64, 4, 2, 1, bias=False),
+            # input is (3) x 32 x 32
+            nn.Conv2d(3, 32, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (64) x 32 x 32
-            nn.Conv2d(64, 64 * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 2),
+            # state size. (32) x 32 x 32
+            nn.Conv2d(32, 32 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32 * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (64*2) x 16 x 16
-            nn.Conv2d(64 * 2, 64 * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 4),
+            # state size. (32*2) x 16 x 16
+            nn.Conv2d(32 * 2, 32 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32 * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (64*4) x 8 x 8
-            nn.Conv2d(64 * 4, 64 * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 8),
+            # state size. (32*4) x 8 x 8
+            nn.Conv2d(32 * 4, 32 * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32 * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (64*8) x 4 x 4
-            nn.Conv2d(64 * 8, 1, 4, 1, 0, bias=False),
+            # state size. (32*8) x 4 x 4
+            nn.Conv2d(32 * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
 
