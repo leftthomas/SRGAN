47488ec00ed1f4d3fec58146ce572b4fc2064a21
leftthomas <leftthomas@qq.com>
2017-12-03 22:58:16 +0800

    remove Image Loss

--- loss.py
+++ loss.py
@@ -19,9 +19,10 @@ class GeneratorLoss(nn.Module):
         # Perception Loss
         perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
         # Image Loss
-        image_loss = self.mse_loss(out_images, target_images)
+        # image_loss = self.mse_loss(out_images, target_images)
 
-        return image_loss + 1e-3 * adversarial_loss + 6e-3 * perception_loss
+        # return image_loss + 1e-3 * adversarial_loss + 6e-3 * perception_loss
+        return 1e-3 * adversarial_loss + 6e-3 * perception_loss
 
 
 if __name__ == "__main__":
