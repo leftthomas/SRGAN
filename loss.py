828ea43df48f7c218aa9a41fb28778f354cb558d
leftthomas <leftthomas@qq.com>
2017-12-06 15:13:13 +0800

    fix bug of TV Loss

--- loss.py
+++ loss.py
@@ -12,6 +12,7 @@ class GeneratorLoss(nn.Module):
             param.requires_grad = False
         self.loss_network = loss_network
         self.mse_loss = nn.MSELoss()
+        self.tv_loss = TVLoss()
 
     def forward(self, out_labels, out_images, target_images):
         # Adversarial Loss
@@ -21,9 +22,28 @@ class GeneratorLoss(nn.Module):
         # Image Loss
         image_loss = self.mse_loss(out_images, target_images)
         # TV Loss
-        tv_loss = (((out_images[:, :, :-1, :] - out_images[:, :, 1:, :]) ** 2 + (
-                out_images[:, :, :, :-1] - out_images[:, :, :, 1:]) ** 2) ** 1.25).mean()
+        tv_loss = self.tv_loss(out_images)
         return image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
+
+
+class TVLoss(nn.Module):
+    def __init__(self, tv_loss_weight=1):
+        super(TVLoss, self).__init__()
+        self.tv_loss_weight = tv_loss_weight
+
+    def forward(self, x):
+        batch_size = x.size()[0]
+        h_x = x.size()[2]
+        w_x = x.size()[3]
+        count_h = self.tensor_size(x[:, :, 1:, :])
+        count_w = self.tensor_size(x[:, :, :, 1:])
+        h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
+        w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
+        return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+
+    @staticmethod
+    def tensor_size(t):
+        return t.size()[1] * t.size()[2] * t.size()[3]
 
 
 if __name__ == "__main__":
