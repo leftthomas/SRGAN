f7ddca07c9d6efebe3c6009a61ec6f0b7c146d97
leftthomas <leftthomas@qq.com>
2017-12-19 16:31:53 +0800

    fix bugs

--- test_video.py
+++ test_video.py
@@ -51,7 +51,6 @@ if __name__ == "__main__":
 
             out = model(image).cpu().data[0]
             out_img = ToPILImage()(out)
-            out_img.show()
             out_img = cv2.cvtColor(np.asarray(out_img), cv2.COLOR_RGB2BGR)
             # save video
             videoWriter.write(out_img)
